b28ee62f0f9587118cc2709a8571b34b12a23140
GuiFMoura <112089708+GuiFMoura@users.noreply.github.com>
2022-08-30 09:08:18 -0400

    ARVORE 2.0

--- arvore.c
+++ arvore.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct nodo{
    int valor;
@@ -19,21 +19,44 @@ void mostraAtual(Nodo *atual){
     if(atual == NULL)
         printf("TEM NODO NAO\n");
     else{
-    printf("   %d   \n", atual->valor);
+    printf(" |%d|\n", atual->valor);
     if(atual->esq == NULL)
-        printf("#\n");
+        printf(" |#| ");
     else
-        printf("%d    \n", atual->esq->valor);
+        printf(" |%d| ", atual->esq->valor);
 
     if(atual->dir == NULL)
-        printf("      #\n");
+        printf(" |#| ");
     else
-        printf("      %d\n", atual->dir->valor);    
+        printf(" |%d| ", atual->dir->valor);
     }
 }
 
-Nodo *inserir(Nodo *atual, int v){
+void mostraArvore(Nodo *p, int nivel){
+    int i;
+    if(p == NULL){
+        //printf(" |#| "); 
+    }
+    else{
+        for(i = 0; i < nivel; i++)
+            printf("  ");
+        printf("%3i\n", p->valor);
+        nivel++;
+        mostraArvore(p->esq, nivel);
+        mostraArvore(p->dir, nivel);
+    }
+}
 
+Nodo* excluir(Nodo *p){
+    if(p == NULL)
+        printf("NODO NAO EXISTE\n");
+    else if(p->esq != NULL || p->dir != NULL){
+        printf("NODO NAO EH UMA FOLHA\n");
+    }else{
+        free(p);
+        p = NULL;
+    }
+    return p;
 
 }
 
@@ -47,6 +70,8 @@ int main(){
         printf("[3]: NAVEGAR PARA A DIREITA \n");
         printf("[4]: INSERIR NOVO NODO \n");
         printf("[5]: NAVEGAR PARA A RAIZ\n");
+        printf("[6]: MOSTRAR ARVORE\n");
+        printf("[7]: EXCLUIR NODO\n");
         printf("[0]: SAIR \n ");
         printf("opcao: ");
         scanf("%d", &op);
@@ -85,15 +110,34 @@ int main(){
                 scanf("%d", &op);
                 if(op == 1 && atual->esq == NULL)
                     atual->esq = criarNodo(v);
-                
+
                 if(op == 2 && atual->dir == NULL)
                     atual->dir = criarNodo(v);
                 mostraAtual(atual);
             }
-            break;  
+            break;
         case 5:
             printf("\nNAVEGANDO PARA A RAIZ");
             atual = raiz;
+            mostraAtual(atual);
+            break;
+        case 6:
+            printf("MOSTRANDO ARVORE");
+            mostraArvore(raiz, 0);
+            break;
+        case 7:
+            if(raiz == NULL){
+                printf("ARVORE VAZIA");
+                break;
+            }
+            printf("[1]: PARA EXCLUIR FILHO ESQUERDA\n[2]: PARA EXCLUIR FILHO DIREITA\n");
+            printf("OPCAO: ");
+            scanf("%d", &op);
+            if(op == 1){
+                atual->esq = excluir(atual->esq);
+            }if(op == 2){
+                atual->dir = excluir(atual->dir);
+            }
             mostraAtual(atual);
             break;
         case 0:
@@ -104,8 +148,8 @@ int main(){
             printf("OPCAO INVALIDA \n");
             break;
         }
-        printf("\n\n\n");
-   }while(op != 0);;
+        printf("\n\n");
+   }while(op != 0);
 
 return 0;
 }
